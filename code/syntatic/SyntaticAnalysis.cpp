103cf3c033f7fcb5f723dca1dd7837b75516cfbd
Francisco <franciscoabreu1408@gmail.com>
2023-10-09 18:15:21 -0300

    Setting the Syntatic class methods

---
+++ code/syntatic/SyntaticAnalysis.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <cstdlib>
+#include <iomanip>
+#include "SyntaticAnalysis.h"
+
+
+SyntaticAnalysis::SyntaticAnalysis(LexicalAnalysis& lex) :
+    m_lex(lex), m_current(lex.nextToken()) {
+
+}
+SyntaticAnalysis::~SyntaticAnalysis() {
+
+}
+
+void SyntaticAnalysis::start() {
+
+}
+
+void SyntaticAnalysis::advance() {
+    m_current = m_lex.nextToken();
+}
+
+void SyntaticAnalysis::eat(enum TokenType type) {
+    std::cout << "Expected (..., " << tt2str(type) 
+              << "), found (\"" << m_current.token << "\", " 
+              << tt2str(m_current.type) << ")";
+    if(type ==  m_current.type) {
+        advance();
+    } else {
+        showError();
+    }
+}
+
+void SyntaticAnalysis::showError() {
+    std::cout << std::setw(2) << std::setfill('0') << m_lex.line() << std::endl;
+
+    switch(m_current.type) {
+        case TT_INVALID_TOKEN:
+        std::cout << "Lexema inválido [" << m_current.token << "]" << std::endl;
+        case TT_UNEXPECTED_EOF:
+        case TT_END_OF_FILE:
+            std::cout << "Fim de arquivo inesperado" << std::endl;
+            break;
+        default:
+            std::cout << "Lexema não esperado [" << m_current.token << "]" << std::endl;
+            break;         
+    }
+    
+    exit(1);
+}
+
