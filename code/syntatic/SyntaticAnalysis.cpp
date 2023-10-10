da05c783305db8cea4ff8caddc4c28ac863877d1
Francisco <franciscoabreu1408@gmail.com>
2023-10-10 00:43:03 -0300

    Implementing the grammer rules

--- code/syntatic/SyntaticAnalysis.cpp
+++ code/syntatic/SyntaticAnalysis.cpp
@@ -49,3 +49,162 @@ void SyntaticAnalysis::showError() {
     exit(1);
 }
 
+// <program> ::= class indentifier [<decl-list>] <body>
+void SyntaticAnalysis::procProgram() {
+    eat(TT_CLASS);
+    eat(TT_ID);
+    
+
+}
+// <decl-list> ::= <decl> “;”{<decl>, “;”}
+void SyntaticAnalysis::procDecl_list() {
+
+}
+
+// <decl> ::= <type> <ident-list>
+void SyntaticAnalysis::procDecl() {
+
+}
+
+// <ident-list> ::= identifier {“,” identifier}
+void SyntaticAnalysis::procIdent_list() {
+
+}
+
+// <type> ::= int | string | float
+void SyntaticAnalysis::procType() {
+    if(m_current.type == TT_INT) {
+        eat(TT_INT);
+    } else if(m_current.type == TT_STRING) {
+        eat(TT_STRING);
+    } else if(m_current.type == TT_FLOAT){
+        eat(TT_FLOAT);
+    } else {
+        showError();
+    }
+
+}
+
+// <body> ::= “{” <stmt-list> “}”
+void SyntaticAnalysis::procBody() {
+     eat(TT_CHAV1);
+     procStmt_list();
+     eat(TT_CHAV2);
+
+}
+
+// <stmt-list> ::=  <stmt> “;” {<stmt> “;”}
+void SyntaticAnalysis::procStmt_list() {
+
+}
+
+//<stmt> ::= <assign-stmt> | <if-stmt> | <do-stmt> | <red-stmt> | <write-stmt>
+void SyntaticAnalysis::procStmt() {
+
+}
+
+// <assign-stmt> ::= identifier “=” <simple_expr>
+void SyntaticAnalysis::procAssign_stmt() {
+
+}
+
+// <if-stmt> ::= if “(” <condition> “)” “{” <stmt-list> “}” <if-stmt’>
+void SyntaticAnalysis::procIf_stmt() {
+
+}
+
+// <if-stmt’> ::= λ | else “{” <stmt-list> “}”
+void SyntaticAnalysis::procIf_stmt_l() {
+
+} 
+
+// <condition> ::= <expression>
+void SyntaticAnalysis::procCondition() {
+
+}
+
+// <do-stmt> ::= do “{” <stmt-list> “}” <do-suffix>
+void SyntaticAnalysis::procDo_stmt() {
+
+}
+
+// <do-suffix> ::= while “(” <condition> “)”
+void SyntaticAnalysis::procDo_suffix() {
+
+}
+
+// <read-stmt> ::= read “(” identifier “)”
+void SyntaticAnalysis::procRead_stmt() {
+
+}
+
+// <write-stmt> ::= write “(” <writable> “)”
+void SyntaticAnalysis::procWrite_stmt() {
+
+}
+
+// <writable> ::= <simple-expr>
+void SyntaticAnalysis::procWritable() {
+
+}
+
+// <expression>	::= <simple-expr> <expression’>
+void SyntaticAnalysis::procExpression() {
+
+}
+
+// <simple-expr’> ::= <addop> <term> <simple-expr’> | λ
+void SyntaticAnalysis::procExpression_l() {
+
+}
+
+// <simple-expr> ::= <term> <simple-expr’>
+void SyntaticAnalysis::procSimple_expr() {
+
+}
+
+// <simple-expr’> ::= <addop> <term> <simple-expr’> | λ
+void SyntaticAnalysis::procSimple_expr_l() {
+
+}
+
+// <term>  ::= <factor-a> <term’>
+void SyntaticAnalysis::procTerm() {
+
+}
+
+// <term’> ::= <mulop> <factor-a> <term’> | λ
+void SyntaticAnalysis::procTerm_l() {
+
+}
+
+// <factor-a> ::= <factor> | “!” <facotr>
+void SyntaticAnalysis::procFactor_a() {
+
+}
+
+// <factor>	::= identifier | <constant> | “(” <expression> “)”
+void SyntaticAnalysis::procFactor() {
+
+}
+
+// <relop> ::= “>” | “>=” | “<” | “<=”  | “!=” | “==”
+void SyntaticAnalysis::procRelop() {
+
+}
+
+// <addop> ::= “+” | “-” | “||”
+void SyntaticAnalysis::procAddop() {
+
+}
+
+// <mulop> ::= “*” | “/” | “&&”
+void SyntaticAnalysis::procMulop() {
+
+}
+
+// <constant> ::= integer_const | literal | real_const
+void SyntaticAnalysis::procConstant() {
+
+}
+
--- code/syntatic/SyntaticAnalysis.h
+++ code/syntatic/SyntaticAnalysis.h
@@ -12,10 +12,40 @@ class SyntaticAnalysis {
     private:
         LexicalAnalysis& m_lex;
         Lexeme m_current;
-        
+
         void advance();
         void eat(enum TokenType type);
         void showError();
+
+        void procProgram();
+        void procDecl_list();
+        void procDecl();
+        void procIdent_list();
+        void procType();
+        void procBody();
+        void procStmt_list();
+        void procStmt();
+        void procAssign_stmt();
+        void procIf_stmt();
+        void procIf_stmt_l(); 
+        void procCondition();
+        void procDo_stmt();
+        void procDo_suffix();
+        void procRead_stmt();
+        void procWrite_stmt();
+        void procWritable();
+        void procExpression();
+        void procExpression_l();
+        void procSimple_expr();
+        void procSimple_expr_l();
+        void procTerm();
+        void procTerm_l();
+        void procFactor_a();
+        void procFactor();
+        void procRelop();
+        void procAddop();
+        void procMulop();
+        void procConstant();
 };
 
 
